d19152958cf0b04139a88215e4074c2fe05be395
aces96 <esraidi12@gmail.com>
2023-04-16 08:48:35 +0000

    changes

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -8,17 +8,12 @@ exports.getAdmin = async (req, res)=>{
     try {
         const getAdmin = await Admin.findOne({username: req.body.username})
 
-            if(getAdmin){
-                if(getAdmin.password == req.body.password){
                     res.status(200).json({
                         done: true,
+                        user: getAdmin
                     })
-                }else{
-                    res.status(400).json({
-                        done: false
-                    })
-                }
-            }
+                
+            
     } catch (error) {
         res.send(error)
     }
